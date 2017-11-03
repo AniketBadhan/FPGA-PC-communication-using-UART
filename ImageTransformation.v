d1e86e3e3891c0cb0b265f6ddf70f4fce521ab36
Aniket Badhan <aniketbadhan@umbc.edu>
2017-11-03 00:37:32 -0400

    Update ImageTransformation.v

--- ImageTransformation.v
+++ ImageTransformation.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 
 module ImageTransformation(
-    input [7:0] old_red,
-    input [7:0] old_green,
-    input [7:0] old_blue,
-	  input clk,
-	  input enable,
-    output reg [7:0] new_red,
-    output reg [7:0] new_green,
-    output reg [7:0] new_blue,
-	  output reg done
+	input [7:0] old_red,
+    	input [7:0] old_green,
+    	input [7:0] old_blue,
+	input clk,
+	input enable,
+    	output reg [7:0] new_red,
+    	output reg [7:0] new_green,
+    	output reg [7:0] new_blue,
+	output reg done
     );
 
 	reg [7:0] tempold_red;
@@ -22,7 +22,6 @@ module ImageTransformation(
 
 	always @ (posedge clk) begin
 		if(enable) begin
-
 			tempold_red <= (old_red >> 3) + (old_red >> 4) + (old_red >> 5);
 			tempold_green <= ((old_green >> 2) + (old_green >> 1));
 			tempold_blue <= (old_blue >> 4);
